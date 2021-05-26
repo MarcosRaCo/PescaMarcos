8347b576e1ad43704eb9c792e70c74fc113616ef
MarcosRaCo <marcosrabcob@gmail.com>
2021-05-26 22:21:46 +0200

    leerUsuarios v1

---
+++ src/Pesca_Marcos_Rabadan.java
@@ -0,0 +1,25 @@
+/**
+ * PACKAGE_NAME
+ * Nombre_project: Pesca_Marcos_Rabadan
+ * Pesca_Marcos_Rabadan
+ * Created by: MarcosRa
+ * Date : 24/05/2021
+ * Description:
+ **/
+public class Pesca_Marcos_Rabadan {
+    public Pesca_Marcos_Rabadan() {
+    }
+
+    public static void imprimirln(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public static void imprimir(String mensaje) {
+        System.out.print(mensaje);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Usuarios();
+        //String path = "src\\usuarios.txt";
+    }
+}
